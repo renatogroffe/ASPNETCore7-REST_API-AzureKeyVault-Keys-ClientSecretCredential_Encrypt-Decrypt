19b72672a2505e201406efab884d7ccec64b07aa
Renato Groffe <renato.groffe@yahoo.com.br>
2023-10-02 21:53:33 -0300

    .NET 7 + ASP.NET Core + REST API + Azure Key Vault + Keys + Autenticação via ClientSecretCredential + Testes de encriptação/decriptação
    .NET 7 + ASP.NET Core + REST API + Azure Key Vault + Keys + Autenticação via ClientSecretCredential + Testes de encriptação/decriptação

---
+++ APICryptoAzureKeyVault/Security/AzureCredentialHelper.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Identity;
+
+namespace APICryptoAzureKeyVault.Security;
+
+// Informacoes sobre os tipos possiveis de Credentials:
+// 
+
+public static class AzureCredentialHelper
+{
+    public static ClientSecretCredential GetClientSecretCredential(
+        IConfiguration configuration) => new ClientSecretCredential(
+            tenantId: configuration["AzureSecurity:TenantId"],
+            clientId: configuration["AzureSecurity:ClientId"],
+            clientSecret: configuration["AzureSecurity:ClientSecret"]);
+}
